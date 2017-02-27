6c0b98199f74196231057314bc27fdca67126686
maspupah <maspupah@spa.gov.my>
2017-02-27 11:24:08 +0800

    Script Download

---
+++ gettingData.R
@@ -0,0 +1,3 @@
+library(readr)
+bptms_Labour_force_by_marital_status_state <- read_csv("C:/Users/User/Downloads/bptms-Labour_force_by_marital_status_state.csv")
+View(bptms_Labour_force_by_marital_status_state)
