4e3eebf5a4aae1e367b004d5e5d4ce023a7fdd72
sakinah <sakinah@spa.gov.my>
2017-02-27 14:46:18 +0800

    semak coding

--- gettingDataNew.R
+++ gettingDataNew.R
@@ -1,4 +1,5 @@
 library()
+# semak coding 
 bptms_Labour_force_by_marital_status_state <- read_csv("http://data.stats.gov.my/download_bptms.php?cat=1&id=34")
 View(bptms_Labour_force_by_marital_status_state)
 
